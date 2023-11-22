e3e71c065c511f3bae3dc40b7cbd18e5f6722dac
ljorgens <leejorgensen22@gmail.com>
2023-11-21 21:08:17 -0800

    clean up

--- src/ModalHeader.js
+++ src/ModalHeader.js
@@ -1,23 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { mapToCssModules, tagPropType } from './utils';
-
-const propTypes = {
-  children: PropTypes.node,
-  /** Add custom class */
-  className: PropTypes.string,
-  /** Custom close button */
-  close: PropTypes.object,
-  closeAriaLabel: PropTypes.string,
-  /** Change underlying component's CSS base class name */
-  cssModule: PropTypes.object,
-  /** Set a custom element for this component */
-  tag: tagPropType,
-  /** Function to be triggered when close button is clicked */
-  toggle: PropTypes.func,
-  wrapTag: tagPropType,
-};
+import { mapToCssModules } from './utils';
 
 function ModalHeader(props) {
   let closeButton;
@@ -58,7 +41,5 @@ function ModalHeader(props) {
     </WrapTag>
   );
 }
-
-ModalHeader.propTypes = propTypes;
 
 export default ModalHeader;
